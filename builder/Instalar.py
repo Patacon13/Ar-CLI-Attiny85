956c19b27df407ff02e27c47b3edc3678e4d45cc
Tomás Assenza <tomyassenza@gmail.com>
2020-08-23 00:28:54 -0300

    Primera prueba
    Primera versión del script funcionando.

---
+++ builder/Instalar.py
@@ -0,0 +1,2 @@
+import os
+os.system('arduino-cli.exe core update-index & arduino-cli.exe core install attiny:avr')
---
+++ builder/main.py
@@ -0,0 +1,6 @@
+import os
+nombre = input("Ingrese nombre de archivo a compilar ")
+nombreSalida = input("Ingrese el nombre del archivo compilar: ")
+#cmd = 'md compilado & DEL .\compilado\\' + nombre+'.c /Q & DEL .\compilado\\'+nombre+'.ino /Q & copy .\\'+nombre+'.c .\compilado\\compilado.ino & cls & arduino-cli compile --fqbn arduino:avr:uno compilado & pause & exit'
+cmd = 'md '+nombreSalida+' & DEL .\\'+nombreSalida+'\\' + nombreSalida+'.c /Q & DEL .\\'+nombreSalida+'\\'+nombreSalida+'.ino /Q & copy .\\'+nombre+'.c .\\'+nombreSalida+'\\'+nombreSalida+'.ino & cls & pause & arduino-cli compile --fqbn arduino:avr:uno '+nombreSalida+' & pause & exit'
+os.system(cmd)
