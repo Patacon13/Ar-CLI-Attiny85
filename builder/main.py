f9e830d5ec6dc770eb9ee34fe9c0b856da0033a9
Tomás Assenza <tomyassenza@gmail.com>
2020-08-23 00:36:31 -0300

    Cambiada placa
    Cambiada placa a Attiny

--- builder/main.py
+++ builder/main.py
@@ -2,5 +2,5 @@ import os
 nombre = input("Ingrese nombre de archivo a compilar ")
 nombreSalida = input("Ingrese el nombre del archivo compilar: ")
 #cmd = 'md compilado & DEL .\compilado\\' + nombre+'.c /Q & DEL .\compilado\\'+nombre+'.ino /Q & copy .\\'+nombre+'.c .\compilado\\compilado.ino & cls & arduino-cli compile --fqbn arduino:avr:uno compilado & pause & exit'
-cmd = 'md '+nombreSalida+' & DEL .\\'+nombreSalida+'\\' + nombreSalida+'.c /Q & DEL .\\'+nombreSalida+'\\'+nombreSalida+'.ino /Q & copy .\\'+nombre+'.c .\\'+nombreSalida+'\\'+nombreSalida+'.ino & cls & pause & arduino-cli compile --fqbn arduino:avr:uno '+nombreSalida+' & pause & exit'
+cmd = 'md '+nombreSalida+' & DEL .\\'+nombreSalida+'\\' + nombreSalida+'.c /Q & DEL .\\'+nombreSalida+'\\'+nombreSalida+'.ino /Q & copy .\\'+nombre+'.c .\\'+nombreSalida+'\\'+nombreSalida+'.ino & cls & arduino-cli compile --fqbn attiny:avr:ATtinyX5 '+nombreSalida+' & pause & exit'
 os.system(cmd)
